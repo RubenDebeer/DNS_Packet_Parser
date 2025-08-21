bfebf0334b2ac30c725aff046b95c5f9011e9bd8
Ruby Tubes De Beer <66409964+RubenDebeer@users.noreply.github.com>
2025-08-21 20:36:58 +0200

    Added Struct representing the packet.

--- dns/main.go
+++ dns/main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello, World!")
+	fmt.Println("Hello, World!")
+}
+
+// So thinking we need some Data Structure to store the Packets
+type Packet struct {
+	PacketBuffer [512]byte
+	position     int
 }
