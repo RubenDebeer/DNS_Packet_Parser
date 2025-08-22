acc386b056aba6eec40b77cb7738e9aedf389040
Ruby Tubes De Beer <66409964+RubenDebeer@users.noreply.github.com>
2025-08-22 13:24:21 +0200

    Added Pointer practice.

--- dns/main.go
+++ dns/main.go
@@ -3,15 +3,36 @@ package main
 import "fmt"
 
 func main() {
-	a := "Hello World"
 
-	fmt.Println("Before modifyString:", a)
-	modifyString(&a)
-	fmt.Println("After modifyString:", a)
-}
+	i, j := 12, 40
 
-// Try and Figure out what is a Pointer and how it works
+	// Print the Values
+	fmt.Println("Print The Values")
+	fmt.Println("i value --> ", i)
+	fmt.Println("j Value --> ", j)
 
-func modifyString(a *string) {
-	*a = "Modified String"
+	// Print the address to the values
+	fmt.Println("Print The address to the values")
+	fmt.Println(" address of I --> ", &i)
+	fmt.Println(" address of J  --> ", &j)
+
+	pi := &i
+	pj := &j
+
+	fmt.Println("Print The address to the values")
+	fmt.Println(" address of I --> ", pi)
+	fmt.Println(" address of J  --> ", pj)
+
+	// What is a the * shit then ?
+
+	fmt.Println("Print the value of the addresses")
+	fmt.Println("Value at address of pi :", *pi)
+	fmt.Println("Value at address of pj :", *pj)
+
+	// *pj --> pi --> i  This is Dereferencing.
+
+	fmt.Println("Change the dereferenced value", *pi)
+	*pi = 800
+	fmt.Println("Change the dereferenced value", *pi)
+
 }
