3c7aa44573f1bc5c082d5af914ecffae434597ad
Ruby Tubes De Beer <66409964+RubenDebeer@users.noreply.github.com>
2025-09-10 15:44:41 +0200

    Uopdate

--- dns/dns_buf/buffer.go
+++ dns/dns_buf/buffer.go
@@ -4,6 +4,7 @@ package dnsbuf
 
 import (
 	"errors"
+	"fmt"
 )
 
 var errByteOutOfRange error = errors.New("ByteOut of range.")
@@ -35,91 +36,64 @@ func (b *ByteBuffer) ChangePosition(position int) {
 }
 
 // Read a Single Byte
-// I added the Explicit return because the Naked return makes me feel Dirty,(Like my code is dirty).
-func (b *ByteBuffer) ReadByte() (returnByte byte, returnError error) {
+func (b *ByteBuffer) ReadByte() (returnByte byte) {
 	if b.readerPosition > len(b.buffer) {
 		returnByte = 0
-		returnError = errByteOutOfRange
-		return returnByte, returnError
+		return returnByte
 	}
 
 	returnByte = b.buffer[b.readerPosition]
-	returnError = nil
 
 	b.readerPosition++
 
-	return returnByte, returnError
+	return returnByte
 }
 
-// get a Single byte without Moving the buffer position
-func (b *ByteBuffer) GetByte() (returnByte byte, returnError error) {
+// Get a Single byte without Moving the buffer position
+func (b *ByteBuffer) GetByte(position int) (returnByte byte) {
 
-	if b.readerPosition > len(b.buffer) {
+	if position > len(b.buffer) {
 		returnByte = 0
-		returnError = errByteOutOfRange
-		return returnByte, returnError
+		return returnByte
 	}
-	returnByte = b.buffer[b.readerPosition]
-	returnError = nil
 
-	return returnByte, returnError
+	returnByte = b.buffer[position]
+	return returnByte
 }
 
 // Get a range of Bytes
-func (b *ByteBuffer) GetByteRange(startPos, copyLength int) (byteRange []byte, byteRangeError error) {
+func (b *ByteBuffer) GetByteRange(startPos, copyLength int) (byteRange []byte) {
 
 	if startPos > len(b.buffer) || startPos < 0 || startPos+copyLength > len(b.buffer) {
 		byteRange = nil
-		byteRangeError = errByteOutOfRange
-		return byteRange, byteRangeError
+		return byteRange
 	}
 
-	byteRange = b.buffer[:]
-	byteRangeError = nil
+	byteRange = b.buffer[startPos : startPos+copyLength]
 
-	return byteRange, byteRangeError
+	return byteRange
 }
 
 // Read 2 bytes as Big-Endian
-func (b *ByteBuffer) ReadU16() (TwoBytes uint16, errRead error) {
-	b1, err := b.ReadByte()
+func (b *ByteBuffer) ReadU16() (TwoBytes uint16) {
+	b1 := b.ReadByte()
 
-	if err != nil {
-		return 0, err
-	}
-
-	b2, err := b.ReadByte()
-	if err != nil {
-		return 0, err
-	}
+	b2 := b.ReadByte()
 
 	TwoBytes = (uint16(b1) << 8) | uint16(b2)
-	errRead = nil
 
-	return TwoBytes, errRead
+	return TwoBytes
 }
 
 // Read 4 bytes as Big-Endian
 func (b *ByteBuffer) ReadU32() (FourBytes uint32, errRead error) {
-	b1, err := b.ReadByte()
-	if err != nil {
-		return 0, err
-	}
+	b1 := b.ReadByte()
 
-	b2, err := b.ReadByte()
-	if err != nil {
-		return 0, err
-	}
+	b2 := b.ReadByte()
 
-	b3, err := b.ReadByte()
-	if err != nil {
-		return 0, err
-	}
+	b3 := b.ReadByte()
 
-	b4, err := b.ReadByte()
-	if err != nil {
-		return 0, err
-	}
+	b4 := b.ReadByte()
 
 	//               32 - 8 == 24          14 - 8 == 16         16 - 8 == 8
 	FourBytes = (uint32(b1) << 24) | (uint32(b2) << 16) | (uint32(b3) << 8) | uint32(b4)
@@ -128,9 +102,43 @@ func (b *ByteBuffer) ReadU32() (FourBytes uint32, errRead error) {
 	return FourBytes, errRead
 }
 
-// A Standard Query looks like
-// QName , QTYPE , QCLASS
+func (b *ByteBuffer) ReadQName(domainName string) (errWrite error) {
 
-func (b *ByteBuffer) ReadQName() (string, error) {
+	// --> [6][google][3][com][0]
+	// --> [0x06][0x67 0x6F 0x6F 0x67 0x6C 0x65][0x03][0x63 0x6F 0x6D][0x00]
+	//[len][bytes][len][bytes]...[0]
+
+	// --> [0x06][0x67 0x6F 0x6F 0x67 0x6C 0x65] | [0x03][0x63 0x6F 0x6D] | [0x00]
+
+	// Create a local position
+	// Read the Original buffer from current position for the length provided in the message
+	// Create a While loop
+	// Get the length
+	// Read the content from the original buffer
+
+	// Read the first byte
+
+	var localPos int = b.Pos()
+	var doneReading bool = false
+
+	for doneReading != false {
+
+		lengthByte := b.GetByte(localPos)
+
+		if lengthByte == 0 {
+			break
+		}
+
+		label := b.GetByteRange(localPos, int(lengthByte))
+
+		if label != nil {
+			domainName += string(label) + "."
+		}
+
+		localPos++
+
+		fmt.Println(label)
+
+	}
 
 }
